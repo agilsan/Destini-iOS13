a4147daa849dce343e9bcb8e040d0fd3c45b681d
agil <antonio_g_s@hotmail.com>
2020-03-12 23:39:05 +0100

    Final

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    var userChoice : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
-
-
+    
+    @objc func updateUI(){
+        let nextStory = storyBrain.nextStory(userChoice: userChoice)
+        storyLabel.text = nextStory.title
+        choice1Button.setTitle(nextStory.choice1, for: UIControl.State.normal)
+        choice2Button.setTitle(nextStory.choice2, for: UIControl.State.normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        userChoice = (sender.titleLabel?.text)!
+        updateUI()
+    }
+    
 }
 
